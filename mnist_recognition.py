43268af1b7dac2906ee455e2b25e8624029a779e
TimDrogin <tim.drogin@gmail.com>
2023-01-15 03:23:07 +0000

    added Activation class

--- mnist_recognition.py
+++ mnist_recognition.py
@@ -5,23 +5,24 @@ from tqdm import tqdm
 
 mnist_config = {
     "layers":[784, 20, 20, 10],
-    "activation":["relu","relu","relu","softmax"],
+    "activaton":["relu","relu","relu","softmax"],
     "learning_rate": 0.0001,
     "epochs":150,
     "random_seed": 1,
     
 }
 
+
+
+
 class Network:
     def __init__(self, config) -> None:
         self.layers = []
         for i in range(len(config["layers"]) - 1):
-            self.layers.append(Layer(config["layers"][i], config["layers"][i+1],config["activation"][i])) #layer initialization with config values 
+            self.layers.append(Layer(config["layers"][i], config["layers"][i+1],config["activaton"][i])) #layer initialization with config values 
 
         self.learning_rate = config["learning_rate"]
         self.epochs = config["epochs"]
-
-
 
     def train(self, train_data):
         pass
@@ -29,28 +30,42 @@ class Network:
     def predict(self):
         pass
 
-class Layer:
-    #class represents o ne layer of the network
 
+
+
+class Layer:
+    
     def __init__(self, inNodes, outNodes, activation_function) -> None:
         self.weights = np.random.rand(inNodes, outNodes)
         self.bias = np.random.rand(outNodes)
-        self.activation = self.get_activation_function(activation_function)
+        self.activaton = Activation(activation_function)
+
+    def feed_forward(self, input_vector):
+        weighted_sum = np.dot(input_vector, self.weights) + self.bias
+        output_vector = self.activaton(weighted_sum)
+        return output_vector
+
+
+    
+        
+
+
+class Activation:
+    #return an objest with.activaton function and its derivative
+    def __init__(self, function_type) -> None:
+        self.get_activation_function(function_type)
 
     def get_activation_function(self, name):
         if name == "sigmoid":
-            return lambda x: 1 / (1 + np.exp(-x))
+            self.activaton =  lambda x: 1 / (1 + np.exp(-x))
+            self.derivative = lambda x: x * (1 - x)
         elif name == "relu":
-            return lambda x: np.maximum(0, x)
+            self.activaton =  lambda x: np.maximum(0, x)
+            self.derivative = lambda x: np.where(x > 0, 1, 0)
         elif name == "tanh":
-            return lambda x: np.tanh(x)
+            self.activaton =  lambda x: np.tanh(x)
+            self.derivative = lambda x: 1 - x ** 2
         elif name == "softmax":
-            return lambda x: np.exp(x) / np.sum(np.exp(x))
-        else:
-            raise ValueError(f"Invalid activation function: {name}")
+            self.activaton =  lambda x: np.exp(x) / np.sum(np.exp(x))
 
-    
-    def feed_forward(self, input_vector):
-        weighted_sum = np.dot(input_vector, self.weights) + self.bias
-        output_vector = self.activation(weighted_sum)
-        return output_vector
+
