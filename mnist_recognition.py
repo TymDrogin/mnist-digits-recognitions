914ec3bc5d735a901a27876e9afa77de6504f786
TimDrogin <tim.drogin@gmail.com>
2023-01-18 02:49:34 +0000

    stage

--- mnist_recognition.py
+++ mnist_recognition.py
@@ -1,7 +1,5 @@
 import numpy as np
 from tqdm import tqdm
-import numbers
-
 
 
 mnist_config = {
@@ -11,56 +9,63 @@ mnist_config = {
     "epochs":150,
 }
 
-if isinstance(mniset_config["random_seed"], numbers.Number):
-    np.random.seed(mnist_config["random_seed"])
-
-
 class Network:
     def __init__(self, config) -> None:
         self.layers = []
         for i in range(len(config["layers"]) - 1):
-            self.layers.append(Layer(config["layers"][i], config["layers"][i+1],config["activation"][i])) 
+            self.layers.append(Layer(config["layers"][i], config["layers"][i+1], config["activation"][i])) 
 
         self.learning_rate = config["learning_rate"]
         self.epochs = config["epochs"]
 
     def train(self, train_data):
-        for i in tqdm(range(self.epochs), desc="Epochs", ncols=80, colour="WHITE", ascii=" >="):
+        for i in tqdm(range(self.epochs - 1), desc="Epochs", ncols=80, colour="WHITE", ascii=" >="):
             pass
+                
+
+    def back_propagation(self, input_vector):
+
+        self.feed_forward(input_vector)
 
 
 
-    def loss_function(prediction, truth_vector):
-        return -np.sum(prediction * np.log(truth_vector))
 
 
+    def feed_forward(self, input_vector):
+        for layer in self.layers:
+            input_vector = layer.activation(layer.weighted_sum(input_vector))
+        return input_vector
+
+    def cross_entropy(prediction, truth_vec):
+        return -np.sum(prediction * np.log(truth_vec))
 
 
 class Layer:
-    
     def __init__(self, inNodes, outNodes, activation_function) -> None:
         self.weights = np.random.rand(inNodes, outNodes)
         self.bias = np.random.rand(outNodes)
-        self.activation = Activation(activation_function)
+        self.get_activation(activation_function)
+     
 
-    def feed_forward(self, input_vector):
+
+    def weighted_sum(self, input_vector):
         weighted_sum = np.dot(input_vector, self.weights) + self.bias
         output_vector = self.activation(weighted_sum)
+
+        self.input = input_vector
+        self.output = weighted_sum
+
         return output_vector
 
+    def gradiient(self):
+        pass
 
 
 
-
-class Activation:
-    #return an objest with.activation function and its derivative
-    def __init__(self, function_type) -> None:
-        self.get_activation_function(function_type)
-
-    def get_activation_function(self, name):
+    def get_activation(self, name):
         if name == "sigmoid":
             self.activation =  lambda x: 1 / (1 + np.exp(-x))
-            self.derivative = lambda x: x * (1 - x)
+            self.derivative =  lambda x: x * (1 - x)
         elif name == "relu":
             self.activation =  lambda x: np.maximum(0, x)
             self.derivative = lambda x: np.where(x > 0, 1, 0)
@@ -70,10 +75,6 @@ class Activation:
         elif name == "softmax":
             self.activation =  lambda x: np.exp(x) / np.sum(np.exp(x))
 
-
-
-
-            
 
 
 
